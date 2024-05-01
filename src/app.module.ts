7a3d9c629b0f002baf066469a263781c98743ffc
SrJasper <lulevita1@hotmail.com>
2024-05-01 12:36:39 -0300

    fix: consumer apply

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StocksModule } from './stocks/stocks.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './user/user.module';
+import { AuthGuardMiddleware } from './auth-guard/auth-guard.middleware';
 
 @Module({
   imports: [StocksModule, UsersModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+
+    consumer.apply(AuthGuardMiddleware).forRoutes("/stocks")
+
+  }
+}
--- src/stocks/stocks.module.ts
+++ src/stocks/stocks.module.ts
@@ -1,16 +1,13 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { StocksService } from './stocks.service';
 import { StocksController } from './stocks.controller';
 import { DatabaseModule } from 'src/database/database.module';
-import { AuthGuardMiddleware } from 'src/auth-guard/auth-guard.middleware';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [StocksController],
   providers: [StocksService],
 })
-export class StocksModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthGuardMiddleware).forRoutes(StocksController);
-  }
+export class StocksModule  {
+
 }
