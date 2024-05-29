4f1d8feb529d6637cc5590036fe0b45ca8cb949e
SrJasper <lulevita1@hotmail.com>
2024-05-29 17:17:56 -0300

    feat: sellprice inflation implemented

--- src/stocks/stocks.service.ts
+++ src/stocks/stocks.service.ts
@@ -147,10 +147,12 @@ export class StocksService {
       console.log('date1: ' + stockSoldInfo.operationDate+ '\ndate2: ' + new Date());
       buyPriceCorrected = await findInflation(stockSoldInfo.operationDate, new Date(), buyPriceRaw);
     } else {//via body
-      sellPrice = stockBodyInfo.sellPrice;
+      console.log(stockBodyInfo.sellPrice);
+      sellPrice = stockBodyInfo.sellPrice * stockSoldInfo.qnt;
+      const dateRegBuy = stockBodyInfo.date === undefined ? new Date() : stockBodyInfo.date;
       
       console.log('date1: ' + stockSoldInfo.operationDate+ '\ndate2: ' + stockBodyInfo.date);
-      buyPriceCorrected = await findInflation(stockSoldInfo.operationDate, stockBodyInfo.date, buyPriceRaw);
+      buyPriceCorrected = await findInflation(stockSoldInfo.operationDate, dateRegBuy, buyPriceRaw);
     }
 
     //const profit = (sellPrice + stockInfo.provents - buyPriceCorrected);
